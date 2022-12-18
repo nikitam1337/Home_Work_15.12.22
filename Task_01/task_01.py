eaea14a1e848878136f8c3a3d0921ebc28446752
Nikita_Lyskov <nikitam.l@mail.ru>
2022-12-18 21:26:30 +0300

    Add Task_01

--- Task_01/task_01.py
+++ Task_01/task_01.py
@@ -3,7 +3,12 @@
 # - 6782 -> 23
 # - 0,56 -> 11
 
-
-
-
-
+userNumber = float(input('Enter a Number: '))
+text = str(userNumber)
+sum = 0
+index = 0
+for i in text:
+    if text[index] != '.':
+        sum += int(text[index])
+    index += 1
+print(f'Sum of digits = {sum}')
